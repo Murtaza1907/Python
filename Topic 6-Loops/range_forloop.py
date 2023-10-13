f0d524a93880e2a809fe2d9d3a1a3685b06f533d
Murtaza1907 <133246065+Murtaza1907@users.noreply.github.com>
2023-10-13 11:44:36 +0500

    Add files via upload

---
+++ Topic 6-Loops/range_forloop.py
@@ -0,0 +1,11 @@
+for i in range(100):
+    print(i)
+    #print from 0 - 99
+
+for i in range(1,50,2):
+    print(i)
+    # print with 2 steps
+
+for i in range(1,50,3):
+    print(i)
+    # print with 3 steps
